a7e5ceeaf99776cbb53b70cff04a047065d337d8
derekmillscanteen <derek.mills@compass-usa.com>
2025-08-06 13:54:47 -0400

    Add files via upload
    fix for vercel

--- server.js
+++ server.js
@@ -1,113 +1,76 @@
-// Import the necessary libraries
 const express = require('express');
-const http = require('http');
-const path = require('path');
-const { Server } = require("socket.io");
-
-// Set up the Express app and HTTP server
 const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+
+// --- In-memory state for the game ---
+const rooms = {};
+
+// Serve the static files from the 'public' directory
+app.use(express.static('public'));
+
+// This is the root route for our app
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-// Serve the static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// A simple in-memory store for our game state
-const rooms = {};
-
-// Helper function to emit the current state to all clients in a room
-function emitStateToRoom(roomName) {
-    if (rooms[roomName]) {
-        io.to(roomName).emit('updateState', rooms[roomName].state);
-    }
-}
-
-// Handle new WebSocket connections
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
-    // Event listener for a client creating a new room
     socket.on('createRoom', ({ room, username }) => {
         if (!rooms[room]) {
             rooms[room] = {
-                state: {
-                    votes: { [username]: null },
-                    revealed: false,
-                    facilitator: username
-                },
-                participants: [username]
+                facilitator: username,
+                votes: { [username]: null },
+                revealed: false
             };
             socket.join(room);
-            console.log(`Room '${room}' created by ${username}.`);
-            emitStateToRoom(room);
-        } else {
-            console.error(`Room '${room}' already exists.`);
+            socket.emit('updateState', rooms[room]);
         }
     });
 
-    // Event listener for a client joining an existing room
     socket.on('joinRoom', ({ room, username }) => {
-        // --- NEW LOGIC HERE ---
-        if (!rooms[room]) {
-            // If the room doesn't exist, create it and set this user as the facilitator
-            rooms[room] = {
-                state: {
-                    votes: { [username]: null },
-                    revealed: false,
-                    facilitator: username
-                },
-                participants: [username]
-            };
+        if (rooms[room]) {
+            rooms[room].votes[username] = null;
             socket.join(room);
-            console.log(`Room '${room}' created on join by ${username}.`);
-        } else {
-            // If the room exists, just add the new user
-            rooms[room].state.votes[username] = null;
-            rooms[room].participants.push(username);
-            socket.join(room);
-            console.log(`User ${username} joined room '${room}'.`);
+            socket.emit('updateState', rooms[room]);
+            io.to(room).emit('updateState', rooms[room]);
         }
-        
-        emitStateToRoom(room);
     });
 
-    // Event listener for a client voting
     socket.on('vote', ({ room, username, vote }) => {
         if (rooms[room]) {
-            rooms[room].state.votes[username] = vote;
-            console.log(`User ${username} voted in room '${room}'.`);
-            emitStateToRoom(room);
+            rooms[room].votes[username] = vote;
+            io.to(room).emit('updateState', rooms[room]);
         }
     });
 
-    // Event listener for revealing votes
     socket.on('revealVotes', ({ room }) => {
-        if (rooms[room] && rooms[room].state.facilitator === rooms[room].participants[0]) {
-             rooms[room].state.revealed = true;
-             console.log(`Votes revealed in room '${room}'.`);
-             emitStateToRoom(room);
-        }
-    });
-    
-    // Event listener for resetting the game
-    socket.on('resetGame', ({ room }) => {
-        if (rooms[room] && rooms[room].state.facilitator === rooms[room].participants[0]) {
-            rooms[room].state.votes = {};
-            rooms[room].state.revealed = false;
-            console.log(`Game reset in room '${room}'.`);
-            emitStateToRoom(room);
+        if (rooms[room]) {
+            rooms[room].revealed = true;
+            io.to(room).emit('updateState', rooms[room]);
         }
     });
 
+    socket.on('resetGame', ({ room }) => {
+        if (rooms[room]) {
+            rooms[room].revealed = false;
+            // Set all votes back to null
+            for (const user in rooms[room].votes) {
+                rooms[room].votes[user] = null;
+            }
+            io.to(room).emit('updateState', rooms[room]);
+        }
+    });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+        console.log('A user disconnected:', socket.id);
+        // We'll handle a more sophisticated cleanup later
     });
 });
 
-// Start the server
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// --- Vercel requires us to export the app instead of listening on a port ---
+module.exports = app;
