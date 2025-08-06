ff05ccfa073ebda2fc44b3e54f4138e5ca5abde6
derekmillscanteen <derek.mills@compass-usa.com>
2025-08-06 14:15:09 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 // --- In-memory state for the game ---
 const rooms = {};
